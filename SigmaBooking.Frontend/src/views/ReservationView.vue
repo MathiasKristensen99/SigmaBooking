e7b56cd91ed39e524e77973f3e6d0615ebde13f9
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2022-05-17 12:05:14 +0200

    Typo in reservationView

--- SigmaBooking.Frontend/src/views/ReservationView.vue
+++ SigmaBooking.Frontend/src/views/ReservationView.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container-fluid">
     <div class="col">
-
-        <div class="row calendar" style="justify-content: center">
-          <div class="col-3" id="kalender-div" style="text-align: center">
-            <Datepicker v-model="date"></Datepicker>
-          </div>
+      <div class="row calendar" style="justify-content: center">
+        <div class="col-3" id="kalender-div" style="text-align: center">
+          <Datepicker v-model="date"></Datepicker>
         </div>
+      </div>
 
       <div class="container">
         <div>
@@ -18,101 +17,98 @@
             <div class="card">
               <div class="card-header">
                 Bord: 1
-                <hr class="solid">
+                <hr class="solid" />
               </div>
-              <div class="card-body">
-                Navn:
-              </div>
+              <div class="card-body">Navn:</div>
               <div class="card-body">
                 Efternavn:
-                <hr class="solid">
+                <hr class="solid" />
               </div>
-              <div class="card-body">
-                ankomst : slut (antal px)
-              </div>
+              <div class="card-body">ankomst : slut (antal px)</div>
               <div class="card-body">
                 Spise
-                <input class="form-check-input" type="checkbox" value="" id="skalSpise">
-                <hr class="solid">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="skalSpise"
+                />
+                <hr class="solid" />
               </div>
               <div class="card-body">
                 Notes:
-                <hr class="solid">
+                <hr class="solid" />
               </div>
 
-
               <div class="card-body">
-                <button class="btn_visMere" data-bs-toggle="collapse" data-bs-target="#visMere">
+                <button
+                  class="btn_visMere"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#visMere"
+                >
                   vis mere
                 </button>
               </div>
               <div id="visMere" class="collapse">
                 <div class="card-body tel">
-                  tel, nr.
-                  123445677
-                  <hr class="solid">
+                  tel, nr. 123445677
+                  <hr class="solid" />
                 </div>
                 <div class="card-body">
-                  Email:
-                  KarlLutz69@gmail.ass
-                  <hr class="solid">
+                  Email: KarlLutz69@gmail.ass
+                  <hr class="solid" />
                 </div>
-                  <div>
-                    <button class="btn btn-success change">
-                      bearbejd
-                    </button>
-                  </div>
                 <div>
-                  <button class="btn btn-danger delete">
-                    slet
-                  </button>
+                  <button class="btn btn-success change">Rediger</button>
+                </div>
+                <div>
+                  <button class="btn btn-danger delete">Slet</button>
                 </div>
               </div>
             </div>
           </div>
-          </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-.kalender-div{
+.kalender-div {
   text-align: center;
 }
-.calendar{
+.calendar {
   margin-top: 110px;
 }
 
-.card{
+.card {
   text-align: center;
   border-radius: 20px;
   border: 2px solid #181818;
 }
-.card-header{
+.card-header {
   -moz-border-radius-topleft: 20px;
   -moz-border-radius-topright: 20px;
   border-bottom: 0px;
   background-color: transparent;
 }
-.card-body{
+.card-body {
   margin-top: -35px;
 }
-.btn_visMere{
+.btn_visMere {
   background-color: transparent;
   border: 0px;
   text-decoration: underline;
 }
-.delete{
+.delete {
   margin-bottom: 10px;
 }
-.change{
+.change {
   margin-top: -35px;
 }
-.tel{
+.tel {
   margin-top: -25px;
 }
-
 </style>
 
 <script>
@@ -126,15 +122,12 @@ export default {
     return {
       date: null,
       reservations: [
-          "manni das mammut",
-          "Rød hår er sekset",
-          "Hækser knepper bedst",
-          "I like turtles"
-      ]
+        "manni das mammut",
+        "Rød hår er sekset",
+        "Hækser knepper bedst",
+        "I like turtles",
+      ],
     };
   },
 };
-
-
-
 </script>
