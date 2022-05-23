ea1213b1105195bd0047a843616d3357272bf8c5
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-05-23 15:45:29 +0200

    Added more tests

---
+++ SigmaBooking.Backend/SigmaBooking.Domain.Test/Services/TableLayoutServiceTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using SigmaBooking.Core.IServices;
+using SigmaBooking.Core.Models;
+using SigmaBooking.Domain.IRepositories;
+using SigmaBooking.Domain.Services;
+using Xunit;
+
+namespace SigmaBooking.Domain.Test.Services;
+
+public class TableLayoutServiceTest
+{
+    #region TableLayoutService init
+
+    [Fact]
+    public void TableLayoutService_IsITableLayoutService()
+    {
+        var repoMock = new Mock<ITableLayoutRepository>();
+        var tableLayoutService = new TableLayoutService(repoMock.Object);
+        Assert.IsAssignableFrom<ITableLayoutService>(tableLayoutService);
+    }
+    #endregion
+
+    [Fact]
+    public void GetTableLayoutByDate()
+    {
+        var expected = new TableLayout
+        {
+            Id = "id1",
+            Date = "01/01/2022",
+            IsDefault = false,
+            Tables = null,
+            TableIds = null
+        };
+
+        var repoMock = new Mock<ITableLayoutRepository>();
+        repoMock.Setup(repository => repository.GetTableLayout(expected.Date)).Returns(expected);
+
+        var tableLayoutService = new TableLayoutService(repoMock.Object);
+        
+        Assert.Equal(expected, tableLayoutService.GetTableLayout("01/01/2022"), new TableLayoutComparer());
+    }
+
+    [Fact]
+    public void DeleteTableLayout()
+    {
+        var tableLayout = new TableLayout
+        {
+            Id = "id1",
+            Date = "01/01/2022",
+            IsDefault = false,
+            Tables = null,
+            TableIds = null
+        };
+
+        var repoMock = new Mock<ITableLayoutRepository>();
+        repoMock.Setup(repository => repository.DeleteTableLayout(tableLayout.Id));
+
+        var tableLayoutService = new TableLayoutService(repoMock.Object);
+        
+        tableLayoutService.DeleteTableLayout(tableLayout.Id);
+        
+        repoMock.Verify(repository => repository.DeleteTableLayout(tableLayout.Id));
+    }
+
+    [Fact]
+    public void CreateTableLayout()
+    {
+        var expected = new TableLayout
+        {
+            Id = "id1",
+            Date = "01/01/2022",
+            IsDefault = false,
+            Tables = null,
+            TableIds = null
+        };
+
+        var repoMock = new Mock<ITableLayoutRepository>();
+        repoMock.Setup(repository => repository.CreateTableLayout(expected)).Returns(expected);
+
+        var tableLayoutService = new TableLayoutService(repoMock.Object);
+        
+        Assert.Equal(expected, tableLayoutService.CreateTableLayout(expected), new TableLayoutComparer());
+    }
+
+    private class TableLayoutComparer : IEqualityComparer<TableLayout>
+    {
+        public bool Equals(TableLayout x, TableLayout y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Id == y.Id && x.IsDefault == y.IsDefault && x.Date == y.Date && x.TableIds.Equals(y.TableIds) && x.Tables.Equals(y.Tables);
+        }
+
+        public int GetHashCode(TableLayout obj)
+        {
+            return HashCode.Combine(obj.Id, obj.IsDefault, obj.Date, obj.TableIds, obj.Tables);
+        }
+    }
+}
--- SigmaBooking.Backend/SigmaBooking.Domain.Test/Services/TableServiceTest.cs
+++ SigmaBooking.Backend/SigmaBooking.Domain.Test/Services/TableServiceTest.cs
@@ -1,5 +1,8 @@
-﻿using Moq;
+﻿using System;
+using System.Collections.Generic;
+using Moq;
 using SigmaBooking.Core.IServices;
+using SigmaBooking.Core.Models;
 using SigmaBooking.Domain.IRepositories;
 using SigmaBooking.Domain.Services;
 using Xunit;
@@ -29,5 +32,70 @@ public class TableServiceTest
             
             // assert
             repoMock.Verify(repository => repository.GetAllTables(), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteTable()
+        {
+            var table = new Table
+            {
+                Id = "id1",
+                Static = false,
+                X = 1,
+                Y = 1,
+                W = 1,
+                H = 1,
+                I = "1"
+            };
+
+            var repoMock = new Mock<ITableRepository>();
+
+            repoMock.Setup(repository => repository.DeleteTable(table.Id));
+
+            var tableService = new TableService(repoMock.Object);
+            
+            tableService.DeleteTable(table.Id);
+            
+            repoMock.Verify(repository => repository.DeleteTable(table.Id));
+        }
+
+        [Fact]
+        public void CreateTable()
+        {
+            var expected = new Table
+            {
+                Id = "id1",
+                Static = false,
+                X = 1,
+                Y = 1,
+                W = 1,
+                H = 1,
+                I = "1"
+            };
+
+            var repoMock = new Mock<ITableRepository>();
+
+            repoMock.Setup(repository => repository.CreateTable(expected)).Returns(expected);
+
+            var tableService = new TableService(repoMock.Object);
+            
+            Assert.Equal(expected, tableService.CreateTable(expected), new TableComparer());
+        }
+
+        public class TableComparer : IEqualityComparer<Table>
+        {
+            public bool Equals(Table x, Table y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return x.Id == y.Id && x.Static == y.Static && x.X == y.X && x.Y == y.Y && x.W == y.W && x.H == y.H && x.I == y.I;
+            }
+
+            public int GetHashCode(Table obj)
+            {
+                return HashCode.Combine(obj.Id, obj.Static, obj.X, obj.Y, obj.W, obj.H, obj.I);
+            }
         }
     }
