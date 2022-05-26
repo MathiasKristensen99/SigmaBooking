523ce6598b39df904caa2dc8e751a9a88e5affb4
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-05-26 11:00:40 +0200

    Update frontend_tests.js

--- SigmaBooking.Frontend/testcafe/frontend_tests.js
+++ SigmaBooking.Frontend/testcafe/frontend_tests.js
@@ -3,15 +3,14 @@ import { ClientFunction } from "testcafe";
 fixture`Navbar tests`;
 
 test.page("https://sigmabooking-68a9f.web.app/")("GoToHomeView", async (t) => {
-  await t.click("#app > header > div > nav > div > button > span");
   await t.click("#navbarToggler > ul > li:nth-child(1) > a");
   const location = await ClientFunction(() => document.location.href);
   await t.expect(location()).contains("/home");
-});
+  }
+);
 test.page("https://sigmabooking-68a9f.web.app/")(
   "GoToReservationView",
   async (t) => {
-    await t.click("#app > header > div > nav > div > button > span");
     await t.click("#navbarToggler > ul > li:nth-child(2) > a");
     const location = await ClientFunction(() => document.location.href);
     await t.expect(location()).contains("/reservation");
