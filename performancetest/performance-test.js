70d39f2fb5894f20871ccba6fd3092d8f8ea2aa1
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-05-26 10:23:01 +0200

    Added performance test

---
+++ performancetest/performance-test.js
@@ -0,0 +1,24 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+
+export const options = {
+    duration: '10s',
+    vus: 50,
+    thresholds: {
+        http_req_duration: ['p(100)<200']
+    },
+};
+
+export default function () {
+    http.get('https://sigmabooking.azurewebsites.net/api/Tables');
+    sleep(3);
+}
+
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
+}
+
